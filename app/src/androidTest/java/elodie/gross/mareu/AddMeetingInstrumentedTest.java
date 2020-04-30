379b1ad18ccf25bc35315d558394499d5fc5d398
Nidow-dev <58668648+Nidow-dev@users.noreply.github.com>
2020-04-30 20:01:54 +0200

    test

--- app/src/androidTest/java/elodie/gross/mareu/AddMeetingInstrumentedTest.java
+++ app/src/androidTest/java/elodie/gross/mareu/AddMeetingInstrumentedTest.java
@@ -1,23 +1,16 @@
 package elodie.gross.mareu;
 
-import android.content.Context;
-import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import androidx.test.espresso.contrib.PickerActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import elodie.gross.mareu.model.Meeting;
@@ -32,9 +25,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static elodie.gross.mareu.service.DummyMeetingGenerator.FAKE_MEETING;
 import static org.hamcrest.Matchers.allOf;
-import static org.junit.Assert.*;
 
 public class AddMeetingInstrumentedTest {
 
@@ -78,48 +69,14 @@ public void checkIfRemovingUserIsWorking() {
 
 /// Ajouter une réunion
 
-    @Test
-    public void addMeeting() {
-        Meeting meeting = new Meeting(1, new Date(), new Date(), "First meeting", mApiMeetingService.getMeetingRoom().get(0), mApiMeetingService.getParticipants());
-        mApiMeetingService.addMeeting(meeting);
-        assertTrue(mApiMeetingService.getMeeting().contains(meeting));
-        assertEquals(1, mApiMeetingService.getMeeting().size());
-    }
+
 
     /// Test la liste de meeting ///
 
-    @Test
-    public void resetMeetingListWithSuccess() {
-        //Add fake meetings to the list
-        ApiMeetingService.getMeeting().addAll(FAKE_MEETING);
-        //Meeting list size list should be 6
-        assertEquals(6, ApiMeetingService.getMeeting().size());
-        //Reset meeting list
-        ApiMeetingService.resetMeeting();
-        assertEquals(0, ApiMeetingService.getMeeting().size());
 
-    }
     /// Test le filtre nom ///
 
-    @Test
-    public void filterByName() {
-        mApiMeetingService.addMeeting(new Meeting(1, new Date(), new Date(), "Meeting n°1", mApiMeetingService.getMeetingRooms().get(0), mApiMeetingService.getParticipants()));
-        mApiMeetingService.addMeeting(new Meeting(2, new Date(), new Date(), "Meeting n°2", mApiMeetingService.getMeetingRooms().get(0), mApiMeetingService.getParticipants()));
 
-        List<MeetingRoom> meetingRooms = new ArrayList<>();
-        meetingRooms.add(mApiService.getMeetingRooms().get(0));
-
-        Meeting meeting_inList = mApiService.getMeetings().get(0);
-        meeting_inList.setMeetingRoom(mApiService.getMeetingRooms().get(0));
-
-        Meeting meeting_notInList = mApiService.getMeetings().get(1);
-        meeting_notInList.setMeetingRoom(mApiService.getMeetingRooms().get(2));
-
-        List<Meeting> meetings = mApiService.getMeetingsByMeetingRoom(meetingRooms);
-        assertTrue(meetings.contains(meeting_inList));
-        assertFalse(meetings.contains(meeting_notInList));
-    }
-}
 
 
 
@@ -129,7 +86,7 @@ public void checkIfRemovingUserIsWorking() {
     public void FilterByHour (){
 
     // from (time)
-    onView(withId(R.id.from)).perform(click());
+    onView(withId(R.id.filtre_date)).perform(click());
     onView(withClassName(Matchers.equalTo(TimePicker.class.getName())))
             .perform(PickerActions.setTime(
             from.get(Calendar.HOUR_OF_DAY),
